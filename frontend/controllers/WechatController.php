bf38adea60fd051c60ea11374b36bda9f4112dae
zinces <305977606@qq.com>
2017-07-03 21:48:34 +0800

    1

--- frontend/controllers/WechatController.php
+++ frontend/controllers/WechatController.php
@@ -5,9 +5,22 @@ use EasyWeChat\Foundation\Application;
 class WechatController extends Controller{
     public $enableCsrfValidation=false;
     public function actionIndex(){
+      $app = new Application(\Yii::$app->params['wechat']);
+        // 从项目实例中得到服务端应用实例。
+        $server = $app->server;
+        $server->setMessageHandler(function ($message) {
+            // $message->FromUserName // 用户的 openid
+            // $message->MsgType // 消息类型：event, text....
+            //return "您好！欢迎关注我!";
+            switch ($message->MsgType){
+                case 'text':
+                    //文本消息
+                    return '你收到的消息：'.$message->Content;
+                    break;
+            }
+        });
 
 
-        $app = new Application(\Yii::$app->params['wechat']);
         $response = $app->server->serve();
 // 将响应输出
         $response->send(); // Laravel 里请使用：return $response;

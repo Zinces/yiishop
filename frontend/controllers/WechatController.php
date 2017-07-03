e92712cbda031d4f68aa7507edac0994733a9c0e
zinces <305977606@qq.com>
2017-07-03 21:24:00 +0800

    1

--- frontend/controllers/WechatController.php
+++ frontend/controllers/WechatController.php
@@ -1,10 +1,15 @@
 <?php
 namespace frontend\controllers;
 use yii\web\Controller;
-
+use EasyWeChat\Foundation\Application;
 class WechatController extends Controller{
     public $enableCsrfValidation=false;
     public function actionIndex(){
-        echo  'wechat-index';
+
+
+        $app = new Application(\Yii::$app->params['wechat']);
+        $response = $app->server->serve();
+// 将响应输出
+        $response->send(); // Laravel 里请使用：return $response;
     }
 }
